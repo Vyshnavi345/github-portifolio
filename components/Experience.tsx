a6b34db6c4072bee63ac510ad3e658ed10744c03
KOYYADOONDYSAIVYSHNAVI <koyya@uchicago.edu>
2025-01-31 15:49:42 -0600

    experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -2,11 +2,12 @@ import Title from "./Title";
 import { MdWork } from "react-icons/md";
 import { SiReactivex, SiToptal, SiFreelancer, SiFiverr } from "react-icons/si";
 import ExperienceCard from "./ExperienceCard";
+import {falkor8Logo} from "./falkor.png"
 
 const Experience = () => {
   return (
     <div className="wrapper">
-      <Title text="Experiences" icon={<MdWork />} />
+      <Title text="Experiences" icon={<Image src={falkor8Logo} alt="Falkor8 Logo" width={30} height={30} />} />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <ExperienceCard
           title="Falkor8"
