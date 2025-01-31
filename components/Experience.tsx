1955f180c6dad34b7b7169ec6606bebc40197573
KOYYADOONDYSAIVYSHNAVI <koyya@uchicago.edu>
2025-01-31 15:57:09 -0600

    experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -3,22 +3,23 @@ import { MdWork } from "react-icons/md";
 import Image from "next/image";
 import { SiReactivex, SiToptal, SiFreelancer, SiFiverr } from "react-icons/si";
 import ExperienceCard from "./ExperienceCard";
-import falkor8Logo from "./falkor.png"
+import falkor8Logo from "./falkor.png";
+import techlogo from "./tech.png"
 
 const Experience = () => {
   return (
     <div className="wrapper">
-      <Title text="Experiences" icon={<Image src={falkor8Logo} alt="Falkor8 Logo" width={30} height={30} />} />
+      <Title text="Experiences" icon={<MdWork />}  />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <ExperienceCard
           title="Falkor8"
           subTitle="Software Engineering Intern July 2024 - September 2024"
-          icon={<SiReactivex />}
+          icon={<Image src={falkor8Logo} alt="Falkor8 Logo" width={30} height={30} />}
         />
         <ExperienceCard
           title="UChicago TechTeam"
           subTitle="Project Member September 2023 - December 2023"
-          icon={<SiToptal />}
+          icon={<Image src={techlogo} alt="Falkor8 Logo" width={30} height={30} />}
         />
         
       </div>
