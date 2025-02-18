2bb090af8e5dea88a072b6048454e0895d6e17ae
KOYYADOONDYSAIVYSHNAVI <koyya@uchicago.edu>
2025-02-17 19:50:12 -0600

    projects

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -25,14 +25,13 @@ const Skills = () => {
           link="https://developer.mozilla.org/en-US/docs/Web/CSS"
         />
         
-        <SkillsInput title="Git" link="https://git-scm.com/" />
-        <SkillsInput title="Github" link="https://github.com/" />
-        <SkillsInput title="Python" link="https://www.python.org/" />
-        <SkillsInput title="SQL" link="https://www.mysql.com/" />
+        <SkillsInput title="Github" link="https://github.com/KOYYADOONDYSAIVYSHNAVI" />
+        <SkillsInput title="Python" link="https://www.linkedin.com/learning/certificates/22a9a9f0c3394469d65454d6802191624487eb33706216f2397483774e3a72a9?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BNPFnNkRHR4u6%2B%2Fa2u7slvw%3D%3D" />
+        <SkillsInput title="SQL" link="https://www.hackerrank.com/certificates/bbc77732aeed" />
         <SkillsInput title="Docker" link="https://www.docker.com/" />
         <SkillsInput title="Flask" link="https://flask.palletsprojects.com/en/stable/" />
         <SkillsInput title="Figma" link="https://www.figma.com/" />
-        <SkillsInput title="AWS" link="https://aws.amazon.com/?nc2=h_lg" />
+        <SkillsInput title="AWS" link="https://www.credly.com/badges/3e1d7338-d635-4b71-8021-f042bd27ba37" />
         <SkillsInput title="TypeScript" link="https://www.typescriptlang.org/" />
         <SkillsInput title="Kubernetes" link="https://kubernetes.io/" />
         <SkillsInput title="Java" link="https://www.java.com/en/" />
